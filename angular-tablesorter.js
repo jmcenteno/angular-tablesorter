9b0f527fd479e832cb0d7135ff4119403d410b2f
jmcenteno <jmcenteno@gmail.com>
2015-05-11 01:46:11 -0400

    - pager widget support

--- angular-tablesorter.js
+++ angular-tablesorter.js
@@ -2,16 +2,27 @@ angular.module('angular-tablesorter', [])
 
 .directive('tablesorter', ['$timeout', function ($timeout) {
 
-	var tpl = '<table><thead><tr></tr></thead><tbody></tbody></table>'
+	var thead = '<thead><tr><th ng-repeat="col in columns">{{col.text}}</th></tr></thead>';
+	var tbody = '<tbody><tr ng-repeat="row in rows"><td ng-repeat="value in row">{{value}}</td></tr></tbody>';
+
+	var pager = '<form class="form-inline">' +
+		'<button type="button" class="first btn btn-sm btn-default">First</button> ' +
+		'<button type="button" class="prev btn btn-sm btn-default">Prev</button> ' +
+		'<span class="pagedisplay"></span> ' +
+		'<button type="button" class="next btn btn-sm btn-default">Next</button> ' +
+		'<button type="button" class="last btn btn-sm btn-default">Last</button> ' + 
+	    '<select class="pagesize form-control input-sm"><option ng-repeat="value in [ 10, 20, 30, 40, 50 ]" value="{{value}}">{{value}}</option></select>' +
+	    '</form>';
+
+	var tfoot = '<tfoot ng-if="showPager"><tr><td colspan="{{columns.length}}" class="pager">' + pager + '</td></tr></tfoot>';
 
 	return {
 		restrict: 'E',
 		replace: true,
-		template: tpl,
-		scope: {},
+		template: '<table>' + thead + tfoot + tbody + '</table>',
 		link: function ($scope, iElement, iAttrs) {
 			
-			var options = $scope.$eval(iAttrs.options)
+			var options = $scope.$eval(iAttrs.config)
 			
 			var config = {
 				widgets: [],
@@ -22,31 +33,55 @@ angular.module('angular-tablesorter', [])
 
 			angular.extend(config, options);
 
+			$scope.showPager = false;
+
 			$scope.$watch(function () {
 			
-				return $scope.$eval(iAttrs.data);
+				return $scope.$eval(iAttrs.tableData);
 			
 			}, function (data) {
 
 				if (angular.isDefined(data)) {
 
+					$scope.columns = [];
+					$scope.rows = [];
+
 					angular.forEach(data[0], function (item, key) {
-						iElement.find('thead tr').append('<th>' + key + '</th>');
+						$scope.columns.push({ text: key });
 					});
 
 					angular.forEach(data, function (item, index) {
 
-						var row = angular.element('<tr/>');
+						var row = [];
 
 						angular.forEach(item, function (value, key) {
-							row.append('<td>' + value + '</td>')
+							row.push(value);
 						});
 
-						iElement.find('tbody').append(row);
+						$scope.rows.push(row);
 
 					});
 
-					iElement.tablesorter(config);
+					if (config.widgets.indexOf('pager') != -1) {
+
+						config.widgetOptions.pager_selectors = {
+							container: '.pager',       		// target the pager markup (wrapper)
+							first: '.first',       			// go to first page arrow
+							prev: '.prev',        			// previous page arrow
+							next: '.next',        			// next page arrow
+							last: '.last',        			// go to last page arrow
+							gotoPage: '.gotoPage',    		// go to page selector - select dropdown that sets the current page
+							pageDisplay: '.pagedisplay', 	// location of where the "output" is displayed
+							pageSize: '.pagesize'     		// page size selector - select dropdown that sets the "size" option
+						}
+
+						$scope.showPager = true;
+
+					}
+
+					$timeout(function () {
+						iElement.tablesorter(config);
+					});
 
 				}
 
